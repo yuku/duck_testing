7e9f79fc5773e46c755b7cc1fd8cac597a692f88
Yuku TAKAHASHI <taka84u9@gmail.com>
2015-08-19 10:38:14 +0900

    Add RKata::Tester#{test_param,test_return} and rename RKata::Tester#test as #match?

--- lib/rkata/tester.rb
+++ lib/rkata/tester.rb
@@ -1,10 +1,46 @@
 module RKata
   class Tester
+    attr_reader :call_data
+
+    # @param [Object] the receiver object.
+    # @param [String] the invoked method's name.
+    def initialize(receiver, method_name)
+      @call_data = MethodCallData.new(receiver, method_name)
+    end
+
+    # @param [Object]
+    # @param [Array<Class>]
+    def test_param(param, expected_types)
+      test(param, expected_types, :param)
+    end
+
+    # @param [Object]
+    # @param [Array<Class>]
+    def test_return(param, expected_types)
+      test(param, expected_types, :return)
+    end
+
     # @param [Object]
     # @param [Array<Class>]
     # @return [true, false]
-    def test(param, expected_types)
+    def match?(param, expected_types)
       !expected_types.all? { |t| !param.is_a?(t) }
+    end
+
+    private
+
+    # @param [Object]
+    # @param [Array<Class>]
+    # @param [Symbol] `:param` or `:return`
+    def test(param, expected_types, type)
+      return if match?(param, expected_types)
+      violation = Violation.new(
+        call_data: call_data,
+        param: param,
+        expected_types: expected_types,
+        param_or_return: type
+      )
+      Reporter::RaiseError.new(violation).report
     end
   end
 end
--- spec/rkata/tester_spec.rb
+++ spec/rkata/tester_spec.rb
@@ -1,20 +1,61 @@
 describe RKata::Tester do
-  let(:tester) { described_class.new }
+  let(:tester) { described_class.new(*constract_param) }
+  let(:constract_param) { [receiver, method_name] }
+  let(:receiver) { nil }
+  let(:method_name) { nil }
 
-  describe "#test" do
-    subject { tester.test(param, expected_types) }
+  shared_context "param matches to expected_types", match: true do
+    let(:param) { 1 }
+    let(:expected_types) { [Float, Fixnum] }
+  end
 
-    context "when param is a one of expected_types" do
-      let(:param) { 1 }
-      let(:expected_types) { [Float, Fixnum] }
+  shared_context "param does not match to expected_types", match: false do
+    let(:param) { 1 }
+    let(:expected_types) { [Float] }
+  end
 
+  describe "#test_param" do
+    subject { tester.test_param(param, expected_types) }
+
+    context "when param matches to expected_types", match: true do
+      it "does not raise error" do
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context "when param does not match to expected_types", match: false do
+      it "raise ContractViolationError" do
+        expect { subject }.to raise_error(RKata::ContractViolationError)
+        expect { subject }.to raise_error(/Contract violation for argument/)
+      end
+    end
+  end
+
+  describe "#test_return" do
+    subject { tester.test_return(param, expected_types) }
+
+    context "when param matches to expected_types", match: true do
+      it "does not raise error" do
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context "when param does not match to expected_types", match: false do
+      it "raise ContractViolationError" do
+        expect { subject }.to raise_error(RKata::ContractViolationError)
+        expect { subject }.to raise_error(/Contract violation for return value/)
+      end
+    end
+  end
+
+  describe "#match?" do
+    subject { tester.match?(param, expected_types) }
+
+    context "when param is a one of expected_types", match: true do
       it { should be true }
     end
 
-    context "when param is not a kind of expected_types" do
-      let(:param) { 1 }
-      let(:expected_types) { [Float] }
-
+    context "when param is not a kind of expected_types", match: false do
       it { should be false }
     end
   end
